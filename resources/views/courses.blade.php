9cf57a94a780840ce5d4fc86564ab9ef3e54e52d
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2023-04-21 16:16:19 +0200

    tags in courses view

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -4,7 +4,11 @@
 
 @section('content')
     @foreach($courses as $course)
-        {{$course->title}} <br>
+        {{$course->title}}<br>
         {{$course}} <br>
+
+        @foreach($course->tags as $tag)
+            {{$tag->name}}
+        @endforeach
     @endforeach
 @endsection
