2efad8c4825d29ee9d599d4275c711801f452113
Tim Bernhard <tim.bernhard@me.com>
2023-05-04 13:46:08 +0200

    refactoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
 use App\Models\Course;
 use App\Models\Subscription;
 use App\Models\Tag;
