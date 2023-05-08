72a3ed44537fb8572976f49d965131c8dc35f235
Tim Bernhard <tim.bernhard@me.com>
2023-05-08 10:21:33 +0200

    Login Tests created

--- tests/Unit/LoginControllerTest.php
+++ tests/Unit/LoginControllerTest.php
@@ -1,24 +1,78 @@
 <?php
 
-
-use App\Http\Controllers\LoginController;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
 
 class LoginControllerTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
 
-    public function testDestroy()
+    /** @test */
+    public function guests_can_view_the_login_form()
     {
+        $response = $this->get('/login');
 
+        $response->assertOk();
+        $response->assertViewIs('login');
     }
 
-    public function testShow()
+    /** @test */
+    public function users_can_log_in_with_valid_credentials()
     {
+        $attributes = [
+            'username' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'password',
+        ];
 
+        $user = User::create($attributes);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($user);
     }
 
-    public function testStore()
+    /** @test */
+    public function users_cannot_log_in_with_invalid_credentials()
     {
+        $attributes = [
+            'username' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'password',
+        ];
 
+        $user = User::create($attributes);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password'
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function authenticated_users_can_log_out()
+    {
+        $attributes = [
+            'username' => 'test',
+            'email' => 'test@test.com',
+            'password' => 'password',
+        ];
+
+        $user = User::create($attributes);
+        $this->actingAs($user);
+
+        $response = $this->post('/logout');
+
+        $response->assertRedirect('/');
+        $this->assertGuest();
     }
 }
