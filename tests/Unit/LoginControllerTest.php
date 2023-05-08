8cbbc61b421efac6bb28bdbadfbd3b76888cf16f
Tim Bernhard <tim.bernhard@me.com>
2023-05-08 09:34:10 +0200

    refactor for xavis test

---
+++ tests/Unit/LoginControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use App\Http\Controllers\LoginController;
+use PHPUnit\Framework\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+
+    public function testDestroy()
+    {
+
+    }
+
+    public function testShow()
+    {
+
+    }
+
+    public function testStore()
+    {
+
+    }
+}
