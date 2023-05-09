47dd0a6f960fe6f1616f2f5c240881a64fe223b2
FireNick44 | YannicA46 <yannic.studer44@gmail.com>
2023-05-09 19:59:18 +0200

    index update, footer fix started

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -18,6 +18,15 @@
                 <dd><a href="{{ url('/search-results') }}">Search</a></dd>
             </dl>
         </div>
+        <div class="socialmedia">
+            <dl>
+                <dt>More inspiration from us</dt>
+                <dd>
+                    <a href="https://discord.gg/QHnc2gCYgw"><img src="{{ asset('svg/logoDiscord.svg') }}"></a>
+                    <a href="https://twitter.com/im_proveyou"><img src="{{ asset('svg/logoTwitter.svg') }}"></a>
+                </dd>
+            </dl>
+        </div>
         <div class="thirdparty">
             <dl>
                 <dt>Disclaimer</dt>
@@ -25,15 +34,6 @@
                     Für diese Webseite sind die Lernenden Tim Bernhard, Demian Flury, Xavier Scherer, Yannic Studer und Elija Zürcher
                     des BBZW Sursee verantwortlich. Es handelt sich hierbei um eine Testversion.
                     Weder die Lernenden noch andere Personen oder Organisationen übernehmen irgendeine Haftung für die Webseite, Inhalte oder Downloads.
-                </dd>
-            </dl>
-        </div>
-        <div class="socialmedia">
-            <dl>
-                <dt>More inspiration from us</dt>
-                <dd>
-                    <a href="https://discord.gg/QHnc2gCYgw"><img src="{{ asset('svg/logoDiscord.svg') }}"></a>
-                    <a href="https://twitter.com/im_proveyou"><img src="{{ asset('svg/logoTwitter.svg') }}"></a>
                 </dd>
             </dl>
         </div>
